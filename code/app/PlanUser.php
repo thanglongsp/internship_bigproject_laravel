06642bb4386e49ef02ef613b714017c878a92258
thanglongsp <thanglongnguyensp@gmail.com>
2021-06-17 22:50:39 +0700

    tesst4

--- code/app/PlanUser.php
+++ code/app/PlanUser.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class PlanUser extends Model
 {
-    // Test1
+    // Test1 2222222222222222222222
     public $timestamps = false;
     protected $table = "plan_user";
 }
