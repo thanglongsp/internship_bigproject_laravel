e3f46322f044ef831849f4326a7dd6b975ed2a0d
thanglongsp <thanglongnguyensp@gmail.com>
2021-06-20 02:55:26 +0700

    yyy

--- code/app/Comment.php
+++ code/app/Comment.php
@@ -6,8 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {	
-	//protected $table = "comments";
-
+   
     public function plan() 
     {
         return $this->belongsTo('App\Plan');
